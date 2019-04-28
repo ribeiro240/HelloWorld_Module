8d844d42dd1efeb1dbf0b7447e5bbbe0921ede80
Filipe Paiva <filipepaiva240@gmail.com>
2019-04-28 20:53:58 -0300

    Create a simple  Hello World module for Magento 2

---
+++ etc/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\FrameWord\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Magento_HelloWorld',
+        __DIR__
+);
